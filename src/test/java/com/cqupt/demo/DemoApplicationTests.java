b266c18fdc5ab9c0b2806e2a3ea280672b1d8d9b
zhuqing-git <750829042@qq.com>
2021-04-22 01:21:06 +0800

    Update DemoApplicationTests.java

--- src/test/java/com/cqupt/demo/DemoApplicationTests.java
+++ src/test/java/com/cqupt/demo/DemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.cqupt.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.lang.System;
+
+
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
