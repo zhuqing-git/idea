a309e12f56a7b1e44e68d413fe93f2095de1af6a
zhuqing-git <750829042@qq.com>
2021-04-22 02:14:14 +0800

    测试成功2222

--- src/main/java/com/cqupt/demo/HelloColtroller.java
+++ src/main/java/com/cqupt/demo/HelloColtroller.java
@@ -1,7 +1,19 @@
 package com.cqupt.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloColtroller {
+
+@RequestMapping("/hello")
+        public String handle01() {
+
+    System.out.println("Hello,Springboot");
+    return "Hello ,Springboot";
+
+        }
+
 
 
 
