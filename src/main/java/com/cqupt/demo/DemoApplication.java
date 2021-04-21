4d9ed183f94dbb777f4ea6069d16f8aeb1c8916d
zhuqing-git <750829042@qq.com>
2021-04-22 02:03:51 +0800

    测试成功

--- src/main/java/com/cqupt/demo/DemoApplication.java
+++ src/main/java/com/cqupt/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-System.out.println("nihoa");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/cqupt/demo/HelloColtroller.java
@@ -0,0 +1,8 @@
+package com.cqupt.demo;
+
+@RestController
+public class HelloColtroller {
+
+
+
+}
